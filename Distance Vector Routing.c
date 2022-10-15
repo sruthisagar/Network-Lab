ad375d068ad3b8ab85607bf5aeda2880b31b63a7
jaz2000 <69115726+jaz2000@users.noreply.github.com>
2022-10-15 23:58:52 +0530

    Removed some edge case errors

--- Distance Vector Routing.c
+++ Distance Vector Routing.c
@@ -1,71 +1,69 @@
 
 //Distance Victor Routing
 //Author :Sruthi Sagar V K
-#include<stdlib.h>
-#include<stdio.h>
-
-struct node
-{
-    int dist[20];
-    int from[20];
-} route[10];
+#include <stdio.h>
 
 int main()
 {
-    int dm[20][20], no;
-    printf("Enter number of nodes\n");
-    scanf("%d",&no);
-    printf("\nEnter distance matrix\n");
-    for (int i = 0; i < no; i++)
+    int n;
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);
+
+    int cost[n][n];
+    printf("Enter the cost matrix\n");
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < no; j++)
+        for (int j = 0; j < n; j++)
         {
-            scanf("%d",&dm[i][j]);
-            dm[i][i] = 0;
-            route[i].dist[j] = dm[i][j];
-            route[i].from[j] = j;
+            scanf("%d", &cost[i][j]);
         }
     }
 
-    int flag;
-    do
+    int next_hop[n][n];
+    for (int i = 0; i < n; i++)
     {
-        flag = 0;
-        for (int i = 0; i < no; i++)
+        for (int j = 0; j < n; j++)
         {
-            for (int j = 0; j < no; j++)
+            next_hop[i][j] = -1;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < n; k++)
             {
-                for (int k = 0; k < no; k++)
+                if (cost[i][j] + cost[j][k] < cost[i][k])
                 {
-                    if ((route[i].dist[j]) > (dm[i][k] + route[k].dist[j]))
-                    {
-                        route[i].dist[j] = route[i].dist[k] + route[k].dist[j];
-                        route[i].from[j] = k;
-                        flag = 1;
-                    }
+                    cost[i][k] = cost[i][j] + cost[j][k];
+                    next_hop[i][k] = j + 1;
                 }
             }
         }
-    } while (flag);
-
-    for (int i = 0; i < no; i++)
-    {
-        printf("\nRouter %d:\n",i+1);
-        printf("Node\t\tNext Hop\t\tDistance\n");
-        for (int j = 0; j < no; j++)
-        {
-            if((j+1)!=(route[i].from[j]+1))
-                printf("%d\t\t%d\t\t%d\n", j+1, route[i].from[j]+1, route[i].dist[j]);
-            else 
-                printf("%d\t\t-\t\t%d\n", j+1, route[i].dist[j]);
-        }
     }
-    return 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("For router %d\n", i + 1);
+        printf("Dest\tnext_hop\tdist\n");
+        for (int j = 0; j < n; j++)
+        {
+            if (next_hop[i][j] == -1)
+            {
+                printf("%d\t-\t\t%d\n", j + 1, cost[i][j]);
+            }
+            else
+            {
+                printf("%d\t%d\t\t%d\n", j + 1, next_hop[i][j], cost[i][j]);
+            }
+        }
+        printf("\n");
+    }
 }
 
-
-// 0 5 2 3 1000
-// 5 0 4 1000 3
-// 2 4 0 1000 4
-// 3 1000 1000 0 1000
-// 1000 3 4 1000 0
+// 0 5 2 3 999
+// 5 0 4 999 3
+// 2 4 0 999 4
+// 3 999 999 0 999
+// 999 3 4 999 0
